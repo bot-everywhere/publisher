cd31c7f0948d4b5727029abf7795cbc4c1812cbd
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-07-22 00:20:16 +0800

    add resolvers

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { GraphQLServer } = require('graphql-yoga')
 const assert = require('assert')
 const dotenv = require('dotenv')
 const log4js = require('log4js')
 const Redis = require('redis')
+const { promisify } = require('util')
+const { GraphQLServer } = require('graphql-yoga')
 
 /* Load config from .env to process.env */
 dotenv.config()
@@ -17,25 +18,43 @@ redis.on('ready', () => logger.debug(`Connected to redis`))
 
 /* Define schema */
 const typeDefs = `
+  type Bot {
+    id: ID!
+    live: Boolean!
+  }
+
   type Mutation {
     ping(id: ID!): Boolean!
   }
+
   type Query {
-    hello(name: String!): String!
+    bot(id: ID!): Bot
   }
 `
 
 /* Define solver */
 const resolvers = {
   Query: {
-    hello(_, { name }) {
-      return `hello ${name}`
-    },
+    bot: (_, { id }, { redis }) => promisify(callback => {
+      redis.multi()
+        .sscan(['bots', 0, 'MATCH', id])
+        .exists([`bots:${id}:live`])
+        .exec((error, replies) => {
+          if (error || !!replies.find(r => r instanceof Redis.RedisError))
+            return callback()
+          const [[cursor, values], live] = replies 
+          if (!values.length) return callback()
+          return callback(null, { id, live: !!live })
+        })
+    })()
   },
   Mutation: {
-    ping() {
-      return true
-    },
+    ping: (_, { id }, { redis }) => promisify(callback => {
+      redis.multi()
+        .sadd(['bots', id])
+        .set([`bots:${id}:live`, 'ok', 'EX', 60])
+        .exec(error => callback(error, true))
+    })()
   }
 }
 
@@ -43,7 +62,9 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-  context: { redis },
+  context: () => ({
+    redis,
+  })
 })
 
 const options = {
